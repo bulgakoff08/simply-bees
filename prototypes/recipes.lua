7108bc48498f38782a93d2f61d4d63eed4404cf2
Mad Engineer <bulgakoff08@gmail.com>
2023-09-12 19:13:16 +0200

    Recipes balance

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -48,7 +48,6 @@ local function recipe(category, subgroup, duration, recipeId, inputs, outputs)
         energy_required = duration,
         ingredients = inputs,
         results = outputs,
-        allow_as_intermediate = false,
         main_product = outputs[1]["name"]
     }
 end
@@ -70,25 +69,25 @@ data:extend({
     recipe("sb-bee-house", "sb-combs", 5, "sb-iron-honey-comb", items("sb-wooden-frame", 1, "sb-iron-worker-bee", 1), items("sb-iron-honey-comb", 5, "sb-broken-frame", 0.3)),
     recipe("sb-bee-house", "sb-combs", 5, "sb-stone-honey-comb", items("sb-wooden-frame", 1, "sb-stone-worker-bee", 1), items("sb-stone-honey-comb", 5, "sb-broken-frame", 0.3)),
     recipe("sb-bee-house", "sb-combs", 5, "sb-uranium-honey-comb", items("sb-wooden-frame", 1, "sb-uranium-worker-bee", 1), items("sb-uranium-honey-comb", 5, "sb-broken-frame", 0.3)),
-    recipe("sb-bee-house", "sb-general", 5, "sb-honey-comb", items("sb-wooden-frame", 1, "sb-worker-bee", 1), items("sb-honey-comb", 5, "sb-broken-frame", 0.5)),
+    recipe("sb-bee-house", "sb-general", 5, "sb-honey-comb", items("sb-wooden-frame", 1, "sb-worker-bee", 1), items("sb-honey-comb", 5, "sb-broken-frame", 0.3)),
 
     recipe("advanced-crafting", "sb-general", 5, "sb-solid-fuel", items("sb-wax", 10), items("solid-fuel", 1)),
 
-    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-coal-piece", items("sb-coal-honey-comb", 1), items("sb-coal-piece", 1, "sb-coal-piece", 0.5, "sb-wax", 0.5)),
-    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-copper-nugget", items("sb-copper-honey-comb", 1), items("sb-copper-nugget", 1, "sb-copper-nugget", 0.5, "sb-wax", 0.5)),
-    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-iron-nugget", items("sb-iron-honey-comb", 1), items("sb-iron-nugget", 1, "sb-iron-nugget", 0.5, "sb-wax", 0.5)),
-    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-stone-piece", items("sb-stone-honey-comb", 1), items("sb-stone-piece", 1, "sb-stone-piece", 0.5, "sb-wax", 0.5)),
-    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-uranium-nugget", items("sb-uranium-honey-comb", 1), items("sb-uranium-nugget", 1, "sb-uranium-nugget", 0.5, "sb-wax", 0.5)),
+    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-coal-piece", items("sb-coal-honey-comb", 1), items("sb-coal-piece", 4, "sb-coal-piece", 0.75, "sb-wax", 0.25)),
+    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-copper-nugget", items("sb-copper-honey-comb", 1), items("sb-copper-nugget", 4, "sb-copper-nugget", 0.75, "sb-wax", 0.25)),
+    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-iron-nugget", items("sb-iron-honey-comb", 1), items("sb-iron-nugget", 4, "sb-iron-nugget", 0.75, "sb-wax", 0.25)),
+    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-stone-piece", items("sb-stone-honey-comb", 1), items("sb-stone-piece", 4, "sb-stone-piece", 0.75, "sb-wax", 0.25)),
+    recipe("sb-centrifuge", "sb-combs", 0.5, "sb-uranium-nugget", items("sb-uranium-honey-comb", 1), items("sb-uranium-nugget", 1, "sb-uranium-nugget", 0.75, "sb-wax", 0.25)),
 
-    recipe("advanced-crafting", "sb-ores", 0.25, "sb-coal", items("sb-coal-piece", 3), items("coal", 1)),
-    recipe("advanced-crafting", "sb-ores", 0.25, "sb-copper-ore", items("sb-copper-nugget", 3), items("copper-ore", 1)),
-    recipe("advanced-crafting", "sb-ores", 0.25, "sb-iron-ore", items("sb-iron-nugget", 3), items("iron-ore", 1)),
-    recipe("advanced-crafting", "sb-ores", 0.25, "sb-stone", items("sb-stone-piece", 3), items("stone", 1)),
-    recipe("crafting-with-fluid", "sb-ores", 0.25, "sb-uranium-ore", items("sb-uranium-nugget", 3, "sulfuric-acid", 2), items("uranium-ore", 1)),
+    recipe("crafting", "sb-ores", 0.25, "sb-coal", items("sb-coal-piece", 4), items("coal", 1)),
+    recipe("crafting", "sb-ores", 0.25, "sb-copper-ore", items("sb-copper-nugget", 4), items("copper-ore", 1)),
+    recipe("crafting", "sb-ores", 0.25, "sb-iron-ore", items("sb-iron-nugget", 4), items("iron-ore", 1)),
+    recipe("crafting", "sb-ores", 0.25, "sb-stone", items("sb-stone-piece", 4), items("stone", 1)),
+    recipe("crafting-with-fluid", "sb-ores", 0.25, "sb-uranium-ore", items("sb-uranium-nugget", 4, "sulfuric-acid", 2), items("uranium-ore", 1)),
 
     recipe("advanced-crafting", "sb-general", 1, "sb-honey-cube", items("sb-honey-comb", 5), items("sb-honey-cube", 1, "sb-wax", 5)),
     recipe("crafting", "sb-general", 1, "sb-wooden-frame", items("wood", 2), items("sb-wooden-frame", 10)),
-    recipe("crafting", "sb-general", 1, "sb-repaired-frame", items("sb-broken-frame", 3), items("sb-wooden-frame", 1)),
+    recipe("crafting", "sb-general", 1, "sb-wooden-repaired-frame", items("sb-broken-frame", 3), items("sb-wooden-frame", 1)),
 
     recipeWithIcon("sb-incubator", "sb-bees", 30, "larva-grow-long", items("sb-larva", 1), items("sb-bee-queen", 1)),
     recipeWithIcon("sb-incubator", "sb-bees", 5, "larva-grow", items("sb-larva", 1, "sb-honey-cube", 5), items("sb-bee-queen", 1)),
@@ -118,11 +117,11 @@ data:extend({
         allow_as_intermediate = false
     },
 
-    recipe("sb-incubator", "sb-bees", 2, "sb-worker-bee", items("sb-bee-queen", 1), items("sb-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
+    recipe("sb-incubator", "sb-bees", 2, "sb-worker-bee", items("sb-bee-queen", 1), items("sb-worker-bee", 32, "sb-larva", 1, "sb-larva", 0.02)),
 
-    recipe("sb-incubator", "sb-resource-bees", 2, "sb-coal-worker-bee", items("sb-bee-queen", 1, "coal", 25), items("sb-coal-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
-    recipe("sb-incubator", "sb-resource-bees", 2, "sb-copper-worker-bee", items("sb-bee-queen", 1, "copper-ore", 25), items("sb-copper-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
-    recipe("sb-incubator", "sb-resource-bees", 2, "sb-iron-worker-bee", items("sb-bee-queen", 1, "iron-ore", 25), items("sb-iron-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
-    recipe("sb-incubator", "sb-resource-bees", 2, "sb-stone-worker-bee", items("sb-bee-queen", 1, "stone", 25), items("sb-stone-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
-    recipe("sb-incubator", "sb-resource-bees", 2, "sb-uranium-worker-bee", items("sb-bee-queen", 1, "uranium-ore", 25), items("sb-uranium-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
+    recipe("sb-incubator", "sb-resource-bees", 2, "sb-coal-worker-bee", items("sb-bee-queen", 1, "coal", 10), items("sb-coal-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
+    recipe("sb-incubator", "sb-resource-bees", 2, "sb-copper-worker-bee", items("sb-bee-queen", 1, "copper-ore", 10), items("sb-copper-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
+    recipe("sb-incubator", "sb-resource-bees", 2, "sb-iron-worker-bee", items("sb-bee-queen", 1, "iron-ore", 10), items("sb-iron-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
+    recipe("sb-incubator", "sb-resource-bees", 2, "sb-stone-worker-bee", items("sb-bee-queen", 1, "stone", 10), items("sb-stone-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
+    recipe("sb-incubator", "sb-resource-bees", 2, "sb-uranium-worker-bee", items("sb-bee-queen", 1, "uranium-ore", 10), items("sb-uranium-worker-bee", 25, "sb-larva", 1, "sb-larva", 0.02)),
 })
