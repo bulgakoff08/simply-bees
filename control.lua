1f831c5573c5c6463c884e78dbd7eb7e7be17cf4
Mad Engineer <bulgakoff08@gmail.com>
2024-10-23 19:08:28 +0200

    Added message when player finds larva in a tree

--- control.lua
+++ control.lua
@@ -3,7 +3,10 @@ script.on_event(defines.events.on_player_mined_entity, function(event)
     if event.entity then
         if string.find(event.entity.name, "tree-") then
             if math.random(1, 100) > 90 then
-                player.get_main_inventory().insert({name = "sb-larva", amount = 1})
+                local inserted = player.get_main_inventory().insert({name = "sb-larva", amount = 1})
+                if inserted > 0 then
+                    player.print({"messages.sb-found-larva"})
+                end
             end
         end
     end
